1bdd3e6e4fa623bae7ff6413c92d8ee703289809
SonNX24042005 <nguyenxuanson24042005@gmail.com>
2025-05-04 08:10:17 +0700

    cấu trúc lại folder

---
+++ qldt/frontend/script.js
@@ -0,0 +1,19 @@
+/* jshint browser: true */
+/* jshint esversion: 6 */
+
+/*let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", function() {
+    var navbar = document.querySelector("nav");
+
+    if (window.scrollY > lastScrollY) {
+        // Nếu cuộn xuống, ẩn navbar
+        navbar.classList.add("hidden");
+    } else {
+        // Nếu cuộn lên, hiện navbar
+        navbar.classList.remove("hidden");
+    }
+
+    lastScrollY = window.scrollY;
+});
+*/
